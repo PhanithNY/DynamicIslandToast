819d7661ace422acbfb7ac7ca00c75c7d42cdbe1
Phanith NY <ny.phanith.fe@gmail.com>
2024-06-16 21:50:55 +0700

    Removed redundant declaration

--- Sources/DynamicIslandToast/Helpers/Config.swift
+++ Sources/DynamicIslandToast/Helpers/Config.swift
@@ -7,21 +7,17 @@
 
 import UIKit
 
-public protocol Config { }
+protocol Config { }
 extension NSObject: Config { }
 
 extension Config where Self: NSObject {
   /// Makes it available to set properties with closures just after initializing.
   ///
-  ///     let label = UILabel().decorate {
+  ///     let label = UILabel().config {
   ///       $0.textAlignment = .center
   ///       $0.textColor = .black
   ///       $0.text = "Hi There!"
   ///     }
-  func decorate(_ closure: (Self) -> Void) -> Self {
-    closure(self)
-    return self
-  }
   
   func config(_ closure: (Self) -> Void) -> Self {
     closure(self)
